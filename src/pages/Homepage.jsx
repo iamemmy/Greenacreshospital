1269d08968c0197786592fded0f97ecccff0f4b6
Samson Eze <38159339+iamemmy@users.noreply.github.com>
2023-05-30 18:23:31 +0100

    commit
    commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "../style/main.css";
 import Hero from "../components/Hero";
 import Intro from '../components/Intro';
+import Services from '../components/Services';
 
 export default function Homepage() {
   return (
     <>
       <Hero />
       <Intro />
+      <Services />
     </>
   )
 }
