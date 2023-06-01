e824d74d63fa167df77a3471ae3bf87410a84ca0
Rosemary Inang <inangrosemary@gmail.com>
2023-06-01 22:41:38 +0100

    Service page
    Working on the service page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Services() {
   return (
-    <div>Services</div>
+    <section className='service-page'>
+        <h1>OUR SERVICES</h1>
+        <p>At Green Acres Hospital, we pride ourselves on providing exceptional service to out patient. From check-in to check-out, our dedicated team is here to make sure you have a great experience whenever you visit our hospital, we are passionate about our patient.</p>
+    
+    </section>
   )
 }
